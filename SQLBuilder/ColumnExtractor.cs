0ff16602022fdbbf9908a54c96f57289d6147647
François-Pierre <francoispierrerousseau.44@gmail.com>
2025-04-27 12:10:04 +0200

    prise en charge des alias

--- SQLBuilder/ColumnExtractor.cs
+++ SQLBuilder/ColumnExtractor.cs
@@ -1,15 +1,25 @@
-﻿using System.Linq.Expressions;
+﻿namespace SQLBuilder;
 
-namespace SQLBuilder;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 public class ColumnExtractor : ExpressionVisitor
 {
     private readonly List<string> _columns = new();
+    private readonly List<string> _aliases = new();
 
     public List<string> Extract(Expression expression)
     {
         Visit(expression);
-        return _columns;
+        var result = new List<string>();
+        for(var i = 0; i < _columns.Count; i++)
+        {
+            if(!string.IsNullOrEmpty(_aliases[i]) && _aliases[i] != GetColumnName(_columns[i]))
+                result.Add($"{_columns[i]} AS {_aliases[i]}");
+            else
+                result.Add(_columns[i]);
+        }
+        return result;
     }
 
     protected override Expression VisitMember(MemberExpression node)
@@ -19,20 +29,32 @@ public class ColumnExtractor : ExpressionVisitor
             var table = param.Type.Name;
             var column = node.Member.Name;
             _columns.Add($"{table}.{column}");
+            _aliases.Add(""); // Pas d'alias trouvé à ce stade
         }
         else
         {
             _columns.Add(node.Member.Name);
+            _aliases.Add("");
         }
         return base.VisitMember(node);
     }
 
     protected override Expression VisitNew(NewExpression node)
     {
-        foreach(var arg in node.Arguments)
+        for(var i = 0; i < node.Arguments.Count; i++)
         {
+            var arg = node.Arguments[i];
+            var alias = node.Members[i].Name; // C'est là qu'on récupère l'alias dans le new { alias = arg }
             Visit(arg);
+            _aliases[this._aliases.Count - 1] = alias;
         }
         return node;
     }
+
+    private string GetColumnName(string fullColumn)
+    {
+        var parts = fullColumn.Split('.');
+        return parts.Length > 1 ? parts[1] : parts[0];
+    }
 }
+
